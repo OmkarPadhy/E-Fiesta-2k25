e13be9e23475c5d1a7838c993ba9f8186dabb4f4
OmkarPadhy <omkarpadhy07@gmail.com>
2025-09-18 20:52:42 +0530

    Changed the sports.jsx

--- frontend/src/Events/sports.jsx
+++ frontend/src/Events/sports.jsx
@@ -58,7 +58,6 @@ const Sports = () => {
     { title: 'Satolia (ECE Only)', image: 'https://upload.wikimedia.org/wikipedia/commons/b/be/Dabba_Kali2.jpg', description: 'Seven stones (traditional Indian outdoor game).', formLink: 'https://forms.gle/BFDkr2QN4KfeFSBi8', comingSoon: false },
     { title: "Smash n' Dash", image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXRnKM7rn9bIKH2g_28DbaISKrGomLfBTpPQ&s', description: 'Badminton matches.', formLink: 'https://forms.gle/Dfh5EXMERiPg9eDQA', comingSoon: false },
     { title: 'Volley Vibes', image: 'https://images.unsplash.com/photo-1461896836934-ffe607ba8211?q=80&w=2000&auto=format&fit=crop', description: 'Volleyball competition.', formLink: 'https://forms.gle/QEQNtDUsq15X98Cj8', comingSoon: false },
-    { title: 'Power Throw', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYe2dPiALo8j0axXj9UOqwIH8r8EQK-WsYWA&s', description: 'Throw ball contest.', formLink: 'https://forms.gle/tRdRZDu25MZhJCVt7', comingSoon: false },
     { title: 'Khel Kabaddi (ECE Only)', image: 'https://sc0.blr1.cdn.digitaloceanspaces.com/article/204120-pjgcfidsfz-1729917365.jpg', description: 'Traditional kabaddi match.', formLink: '#', comingSoon: true },
     { title: 'Street Champs (Gully Football)', image: 'https://images.unsplash.com/photo-1531415074968-036ba1b575da?q=80&w=2067&auto=format&fit=crop', description: 'Gully football event.', formLink: '#', comingSoon: true }
   ];
@@ -66,6 +65,7 @@ const Sports = () => {
   const girlsEvents = [
     { title: 'Gully Cricket', image: 'https://images.unsplash.com/photo-1709233955550-1a316eab4974?q=80&w=1974&auto=format&fit=crop', description: 'Street cricket event.', formLink: 'https://forms.gle/FYZnVtbfFMXnVfoG6', comingSoon: false },
     { title: 'Checkmate Clash', image: 'https://images.unsplash.com/photo-1529699211952-734e80c4d42b?q=80&w=2071&auto=format&fit=crop', description: 'Chess tournament.', formLink: 'https://forms.gle/CZQdsSx4RzujqApN8', comingSoon: false },
+     { title: 'Power Throw', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYe2dPiALo8j0axXj9UOqwIH8r8EQK-WsYWA&s', description: 'Throw ball contest.', formLink: 'https://forms.gle/tRdRZDu25MZhJCVt7', comingSoon: false },
     { title: 'Striker Shots', image: 'https://images.unsplash.com/photo-1652558973183-a3f046921163?q=80&w=2081&auto=format&fit=crop', description: 'Carrom board competition.', formLink: 'https://forms.gle/54RgfxgosCAW1fVc8', comingSoon: false },
     { title: 'Rope Rumble', image: 'https://images.unsplash.com/photo-1692366850335-9415ee4724c8?q=80&w=2166&auto=format&fit=crop', description: 'Tug of war competition.', formLink: 'https://forms.gle/HAz6LAmUVeHXhHjk8', comingSoon: false},
     { title: 'Zesty Race', image: 'https://m.media-amazon.com/images/I/71oJ4Nl22OL._UF350,350_QL80_.jpg', description: 'Fun relay or sprint race (girls only).', formLink: 'https://forms.gle/RiNgqBFEWaMgs6fd9', comingSoon: true },
